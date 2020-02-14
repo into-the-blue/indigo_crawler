5f2b2622ca9b0a0e1dab891b593e3198ea5ec5e6
Origami <q@timvel.com>
2020-02-14 10:42:16 +0800

    scroll to bottom

--- urlCrawler/src/crawler.py
+++ urlCrawler/src/crawler.py
@@ -141,6 +141,8 @@ class UrlCrawler(object):
                 try:
                     sleep(2)
                     logger.info('Get urls in page, current {}'.format(i+1))
+                    driver.execute_script(
+                        "window.scrollTo(0, document.body.scrollHeight);")
                     urls = self.get_urls_in_page(station_info)
                     url_saved = self.on_get_new_urls(urls, station_info)
                     if not url_saved:
