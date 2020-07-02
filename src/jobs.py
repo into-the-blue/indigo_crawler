af46a048561f548efe104350a6fbffc2f0153f2e
Origami <q@timvel.com>
2020-07-02 21:25:58 +0800

    stop crawl url if too many tasks

--- src/jobs.py
+++ src/jobs.py
@@ -59,9 +59,9 @@ MAXIMAL_NUMBER_OF_TASKS = 3600*4
 
 
 def crawl_by_district():
-    # num_of_idle = db_ins.get_num_of_idle_tasks()
-    # if num_of_idle >= MAXIMAL_NUMBER_OF_TASKS:
-    #     return
+    num_of_idle = db_ins.get_num_of_idle_tasks()
+    if num_of_idle >= MAXIMAL_NUMBER_OF_TASKS:
+        return
     for city in CITIES:
         ins = UrlCrawler()
         ins.setup_city_and_source(city)
@@ -72,9 +72,9 @@ def crawl_by_district():
 
 
 def crawl_by_metro_station():
-    # num_of_idle = db_ins.get_num_of_idle_tasks()
-    # if num_of_idle >= MAXIMAL_NUMBER_OF_TASKS:
-    #     return
+    num_of_idle = db_ins.get_num_of_idle_tasks()
+    if num_of_idle >= MAXIMAL_NUMBER_OF_TASKS:
+        return
     for city in CITIES:
         ins = UrlCrawler()
         ins.setup_city_and_source(city)
@@ -94,15 +94,15 @@ def on_finish_url_crawling(taskname=URL_CRAWLER_TASK_BY_LATEST, url_count=0, cit
 
 
 def enqueue_url_crawler(_city=None):
-    # num_of_idle = db_ins.get_num_of_idle_tasks()
-    # if num_of_idle >= MAXIMAL_NUMBER_OF_TASKS:
-    #     logger.warning(
-    #         'Too many tasks: {}'.format(num_of_idle))
-    #     delayed = math.floor(num_of_idle/3600/4)
-    #     q_url_crawler.enqueue_at(
-    #         datetime.now()+timedelta(minutes=delayed*60), enqueue_url_crawler, args=(_city,))
-    #     logger.warning('enqueued, execute after {}h'.format(delayed))
-    #     return
+    num_of_idle = db_ins.get_num_of_idle_tasks()
+    if num_of_idle >= MAXIMAL_NUMBER_OF_TASKS:
+        logger.warning(
+            'Too many tasks: {}'.format(num_of_idle))
+        delayed = math.floor(num_of_idle/3600/4)
+        q_url_crawler.enqueue_at(
+            datetime.now()+timedelta(minutes=delayed*60), enqueue_url_crawler, args=(_city,))
+        logger.warning('enqueued, execute after {}h'.format(delayed))
+        return
     _cities = CITIES
     if _city:
         _cities = [_city]
